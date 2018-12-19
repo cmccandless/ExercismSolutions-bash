eae953f020246687c0607468effa0dfb6256102e
Corey McCandless <corey.mccandless@atlasied.com>
2018-12-19 15:57:10 -0500

    bash: new revisions

--- grains/grains.sh
+++ grains/grains.sh
@@ -7,19 +7,13 @@ main() {
   input="${1}"
 
   if [ "$input" == "total" ]; then
-    output=$(main 64)
-    bc <<< "$output + $output - 1"
+    bc <<< "2 ^ (64) - 1"
   else
     if [[ $input -le 0 ]] || [[ $input -gt 64 ]]; then
       echo 'Error: invalid input'
       return 1
     fi
-    output=1
-    while [[ $input -gt 1 ]]; do
-      output=$(bc <<< "$output * 2")
-      (( input-- ))
-    done
-    echo "$output"
+    bc <<< "2 ^ ($input - 1)"
   fi
 }
 
