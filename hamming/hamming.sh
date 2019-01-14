c43a7f7c87837eb2092d2e935e917ae50f39c63d
Corey McCandless <corey.mccandless@atlasied.com>
2019-01-14 11:38:51 -0500

    bash: new revisions

--- hamming/hamming.sh
+++ hamming/hamming.sh
@@ -6,21 +6,22 @@ set -o nounset
 main() {
   if [[ ${#@} -lt 2 ]]; then
     printf "Usage: hamming.sh <strand1> <strand2>"
-  else
-    left="${1}"
-    right="${2}"
-    if [[ ${#left} != ${#right} ]]; then
-      printf "left and right strands must be of equal length"
-    else
-      score=0
-      for (( i=0; i<"${#left}"; i++ )); do
-        [[ ${left:i:1} != ${right:i:1} ]] && (( score += 1))
-      done
-      printf "%d" "$score"
-      return 0
-    fi
+    return 1
   fi
-  return 1
+  
+  left="${1}"
+  right="${2}"
+  if [[ ${#left} != ${#right} ]]; then
+    printf "left and right strands must be of equal length"
+    return 1
+  fi
+
+  score=0
+  for (( i=0; i<"${#left}"; i++ )); do
+    [[ ${left:i:1} != ${right:i:1} ]] && (( score += 1))
+  done
+  printf "%d" "$score"
+  return 0
 }
 
 main "$@"
