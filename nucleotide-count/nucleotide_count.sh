9a8e05e28036090cc765aaa6d8597f02c9e5cb81
Corey McCandless <corey.mccandless@atlasied.com>
2019-02-04 09:30:50 -0500

    new solutions: nucleotide-count

---
+++ nucleotide-count/nucleotide_count.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+nucleotides='A C G T'
+
+valid()
+{
+  filt="$1"
+  for n in $nucleotides; do
+    filt="${filt//$n}"
+  done
+  [ -z "$filt" ]
+}
+
+main() {
+  input="${1:-}"
+  if ! valid "$input"; then
+    echo "Invalid nucleotide in strand"
+    return 1
+  fi
+
+  for nucleotide in $nucleotides; do
+    filt="$input"
+    for not in $nucleotides; do
+      [[ $nucleotide != $not ]] && filt="${filt//$not}"
+    done
+    echo "$nucleotide: ${#filt}"
+  done
+}
+
+main "$@"
