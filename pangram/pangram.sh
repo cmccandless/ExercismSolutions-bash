6632eea53f28bb01e355dee29b67c67049e50c69
Corey McCandless <corey.mccandless@atlasied.com>
2019-02-18 15:33:54 -0500

    pangram: extract bulky logic to variable

--- pangram/pangram.sh
+++ pangram/pangram.sh
@@ -3,12 +3,17 @@
 set -o errexit
 set -o nounset
 
+count_unique_letters()
+{
+  tr '[:upper:]' '[:lower:]'<<<"${1:-}" |
+    fold -w1 |
+    sort -u |
+    tr -cd '[:alpha:]' |
+    wc -c
+}
+
 main() {
-  [ "$(tr '[:upper:]' '[:lower:]'<<<"${1:-}" | \
-    fold -w1 | \
-    sort -u | \
-    tr -cd '[:alpha:]' | \
-    wc -c)" -eq 26 ] && echo "true" || echo "false"
+  [ "$(count_unique_letters "$1")" -eq 26 ] && echo "true" || echo "false"
 }
 
 main "$@"
