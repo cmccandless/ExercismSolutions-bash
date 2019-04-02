c238f12407e58d1534516fa724141dd922dd13c5
Corey McCandless <corey.mccandless@atlasied.com>
2019-04-02 09:31:31 -0400

    roman-numerals: new solution

---
+++ roman-numerals/roman_numerals.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+main() {
+  number="${1}"
+  
+  while read -a args; do
+    digit="${args[0]}"
+    value="${args[1]}"
+    while [ "$number" -ge "$value" ]; do
+      printf '%s' "$digit"
+      number=$(( number - value ))
+    done
+  done <<EOL
+M 1000
+CM 900
+D 500 
+CD 400 
+C 100
+XC 90
+L 50
+XL 40
+X 10
+IX 9
+V 5
+IV 4
+I 1
+EOL
+  printf '\n'
+}
+
+main "$@"
