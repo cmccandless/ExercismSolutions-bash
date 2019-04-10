de43c9af848b279b0af921ac43eafe100bb96f06
Corey McCandless <corey.mccandless@atlasied.com>
2019-04-10 11:08:52 -0400

    atbash-cipher: new revision

--- atbash-cipher/atbash_cipher.sh
+++ atbash-cipher/atbash_cipher.sh
@@ -1,38 +1,58 @@
 #!/usr/bin/env bash
+#
+# Encode/Decode text using Atbash Cipher
+#
+# usage: atbash_cipher.sh {encode, decode} <text>
 
 set -o errexit
 set -o nounset
 
 REV=( {z..a} )
 
-# https://unix.stackexchange.com/a/92448
+#######################################
+# Get ASCII code for character
+# Arguments:
+#   char    single character
+# Source: https://unix.stackexchange.com/a/92448
+#######################################
 ord() {
-  LC_CTYPE=C printf '%d' "'$1"
+  char="$1"
+  LC_CTYPE=C printf '%d' "'$char"
 }
 
 START="$(ord a)"
 
+#######################################
+# Sanitize input text, removing non-
+# alphanumeric characters and transforming
+# alphabetic characters to lowercase.
+#######################################
 clean() {
   input="${1:-}"
   input="${input//[^[:alpha:][:digit:]]/}"
   echo "${input,,}"
 }
 
+#######################################
+# Insert a space every $chunksize characters
+#######################################
 addSpacing() {
-  timeout=0.5
-  chunksize=5
-  read -r -t "$timeout" -n "$chunksize" -s chunk
+  chunksize="${1:-5}"
+  read -r -n "$chunksize" -s chunk
   printf '%s' "$chunk"
-  while read -r -t "$timeout" -n "$chunksize" -s chunk; do
+  while read -r -n "$chunksize" -s chunk; do
     if [ -n "$chunk" ]; then
       printf ' %s' "$chunk"
     fi
   done
 }
 
+#######################################
+# Implementation of `tr [:alpha:] $(rev<<<[:alpha:])`,
+#######################################
 transcode() {
   output=''
-  while read -r -t 0.5 -n 1 -s ch; do
+  while read -r -n 1 -s ch; do
     if [ -n "$ch" ]; then
       if [[ "$ch" =~ [a-z] ]]; then
         ch="${REV[$(( $(ord "$ch") - START ))]}"
@@ -46,7 +66,7 @@ transcode() {
 
 main() {
   case "$1" in
-    encode) clean "$2" | transcode | addSpacing ;;
+    encode) clean "$2" | transcode | addSpacing 5 ;;
     decode) clean "$2" | transcode;;
   esac
 }
